b3c5711d7d0649c1d8f3ca743b9c9f796a97a099
SayedMudassir Shahid <sayedmudassir.10725380@ltimindtree.com>
2023-10-19 13:41:26 +0530

    redirect params and query for cityview route

--- weather_app/src/views/HomeView.vue
+++ weather_app/src/views/HomeView.vue
@@ -21,6 +21,7 @@
             v-for="searchResult in mapboxSearchResults"
             :key="searchResult.id"
             class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
           >
             {{ searchResult.place_name }}
           </li>
@@ -33,7 +34,22 @@
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+const previewCity = (searchResult) => {
+  const [city, state] = searchResult.place_name.split(',')
+  router.push({
+    name: 'cityView',
+    params: { state: state.replaceAll(' ', ''), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true
+    }
+  })
+}
 const mapboxAPIKey =
   'pk.eyJ1IjoibXVkYXNzaXJzYXllZC0xMTAiLCJhIjoiY2xudTd0Nmc2MDhzYjJscGE3bGwyZHFlbyJ9._pJLkAlzMVcET6vmesYCSg'
 const searchQuery = ref('')
